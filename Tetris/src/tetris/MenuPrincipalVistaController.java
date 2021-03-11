236d7fe8b7792319f7a1ec733a795502bf4ea934
emanuel-benjumea <66340501+emanuel-benjumea@users.noreply.github.com>
2021-03-10 22:32:15 -0500

    ventana registrar
    se agrego 1 imagen, mas hace que valla desde la ventana principal a la ventana registrar

--- Tetris/src/tetris/MenuPrincipalVistaController.java
+++ Tetris/src/tetris/MenuPrincipalVistaController.java
@@ -5,14 +5,19 @@
  */
 package tetris;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -29,15 +34,27 @@ public class MenuPrincipalVistaController implements Initializable {
     private Button play;
     @FXML
     private Button botonModoOscuro1;
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
     private void modoOscuro(ActionEvent event) {
-        
+
     }
-    
+
+    //esto abre la ventana regisrar
+    @FXML
+    private void btregistrar(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/tetris/VentanaRegistro.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("Ayuda");
+        stage.setScene(new Scene(root1));
+        stage.show();
+
+    }
 }
---
+++ Tetris/src/tetris/VentanaRegistroController.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetris;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Asus
+ */
+public class VentanaRegistroController implements Initializable {
+
+    @FXML
+    private Button botonRegistrar;
+    @FXML
+    private TextField IngresarUsuario;
+    @FXML
+    private TextField agergarContraseña;
+    @FXML
+    private TextField repetirContraseña;
+    @FXML
+    private ImageView imagenRegistro;
+    @FXML
+    private ImageView imagentetris;
+    @FXML
+    private Button botonAtras;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        //imagenes de la interfaz
+        Image ima = new Image("/recursos/imagenregistro.jpg");
+        Image titulo = new Image("/recursos/Titulo.png");
+        imagenRegistro.setImage(ima);
+        imagentetris.setImage(titulo);
+        
+        
+    }
+// esto cierra la ventana 
+    @FXML
+    private void btatras(ActionEvent event) {
+           Stage stage = (Stage) this.botonAtras.getScene().getWindow();
+        stage.close();
+    }
+
+
+}
